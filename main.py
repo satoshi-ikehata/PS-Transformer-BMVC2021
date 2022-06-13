52442dfef8d80d037b1e4b88e8b287d6b4e57669
Satoshi Ikehata <satoshi.ikehata@gmail.com>
2022-06-13 14:13:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -30,41 +30,10 @@ def main():
     epochs = 1
     for epoch in range(epochs):
         print(f'Run {epoch}-th epoch')
-        # print(f'Number of Training Sets {trainData.data.get_num_set()}, Number of Training Objs {trainData.data.get_num_object()}')
         print(f'Number of Test Sets {testData.data.get_num_set()}, Number of Objs {testData.data.get_num_object()}')
         losses = 0
         errors = 0
-
-        # """TRAINING"""  (Under Construction)       
-        # """ HARD SAMPLES (INDIRECT) """
-        # trainData.reset()      
-        # objids = np.random.permutation(trainData.data.get_num_object())[:5]
-        # for k in objids:
-        #     trainData.load(objid=k,objset=2)
-        #     trainData.load(objid=k,objset=3)
-        # trainObj.run('Train', data=trainData, batch_size=64)
-
-        # """ EASY SAMPLES (DIRECT) """
-        # trainData.reset()
-        # objids = np.random.permutation(trainData.data.get_num_object())[:5]
-        # for k in objids:
-        #     trainData.load(objid=k,objset=0)
-        #     trainData.load(objid=k,objset=1)
-        # loss, error, output = trainObj.run('Train', data=trainData, batch_size=64)
-        # losses += loss
-        # errors += error
-
-        # output = output.permute(1, 2, 0, 3).reshape(1, output.shape[1], output.shape[2], -1)
-        # tensorboard_writer.add('[Train]　Normal Maps', output, trainObj.global_iter, 'Image')
-        # tensorboard_writer.add('[Train] Loss (Object)', loss, trainObj.global_iter, 'Scalar')
-        # tensorboard_writer.add('[Train] Error (Object)', error, trainObj.global_iter, 'Scalar')
-
-        # tensorboard_writer.add('Train Loss (Epoch)', losses, epoch, 'Scalar')
-        # tensorboard_writer.add('Train Error (Epoch)', errors, epoch, 'Scalar')
-
-        # """Save Chackpoint"""
-        # trainObj.net.save_models(args.checkpoint)
-
+        
         """TEST"""
         outputs = []
         errors = 0
